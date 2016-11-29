004017ed2309c6715dffdf31670f206b9ca705ef
Matt Urenovich <matt.urenovich@gmail.com>
2016-11-29 14:32:01 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meteor-Book-Tutorials
+-------
+Tutorials and Modules from Meteor: Full-Stack Web Application Development from O'Reilly Safari Online Books
---
+++ module-1/my-meteor-blog/client/templates/home.js
@@ -0,0 +1,26 @@
+Template.home.created = function(){
+  console.log('Created the home template');
+};
+Template.home.rendered = function(){
+  console.log('Rendered the home template');
+
+  this.find('p').innerHTML = 'We just replaced that text!';
+};
+
+Template.home.destroyed = function(){
+  console.log('Destroyed the home template');
+};
+
+Template.home.helpers({
+  exampleHelper: function(){
+    return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong>.');
+  },
+  dataContextHelper: function(){
+      return {
+        someText: 'This text was set using a helper of the parent template.',
+        someNested: {
+          text: 'That comes from "someNested.text"'
+        }
+      };
+    }
+});
